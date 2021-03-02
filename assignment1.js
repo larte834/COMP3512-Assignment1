a791ca3129b3b3880bb8d39cdad6b66398efd28c
larte834 <larte834@mtroyal.ca>
2021-03-02 15:00:50 -0700

    comp 3512

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    
+    //header();
+
+        //set header text
+        const info = document.querySelector('div.a');
+        info.textContent = "COMP 3512 Assign1";
+
+        function header() {
+            const info = document.querySelector('div.a');
+            document.querySelector("div.a").style.display = "grid";
+            //create elements to be displayed
+            const credits = document.createElement('label');
+            const name = document.createElement('label');
+            const course = document.createElement('label');
+            const party = document.createElement('label');
+
+            //create text content
+            credits.textContent = "Credits:"
+            name.textContent = "Randy Lam & Lidiya Artemenko";
+            course.textContent = "Comp 3512";
+            party.textContent = "";               
+
+            //append labels to the div
+            info.appendChild(credits);
+            info.appendChild(name);
+            info.appendChild(course);
+            info.appendChild(party);
+        }
+
+        info.addEventListener('mouseover', (e) => {  
+            //show header info
+            header()
+ 
+            //hide header info after 5 sec
+            setTimeout( function () {
+                header().style.display='none';
+            }, 5000);
+        })
+
 });
