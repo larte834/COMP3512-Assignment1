4f97e7c7f39fe7fb7f564680c691d9b48d0a057d
landyram <59937297+landyram@users.noreply.github.com>
2021-03-02 19:23:06 -0700

    Add files via upload
    Test. VS code extensions triggering me

--- assignment1.js
+++ assignment1.js
@@ -3,8 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
         //set header text
         const info = document.querySelector('div.a');
+        const title = document.createElement('label');
+        title.appendChild(title);
         info.textContent = "COMP 3512 Assign1";
 
+        /*
         function header() {
             const info = document.querySelector('div.a');
             document.querySelector("div.a").style.display = "grid";
@@ -27,13 +30,36 @@ document.addEventListener("DOMContentLoaded", function() {
             info.appendChild(party);
         }
 
+        */
+       
         info.addEventListener('mouseover', (e) => {  
             //show header info
-            header()
+            //header()
+            const section = document.createElement('section');
+            info.appendChild(section);
+            section.style.display = "grid";
+            // create elements to be displayed
+            const credits = document.createElement('label');
+            const name = document.createElement('label');
+            const course = document.createElement('label');
+            const party = document.createElement('label');
+
+            // create text content
+            credits.textContent = 'Credits';
+            name.textContent = 'Randy Lam & Lidiya Artemenko';
+            course.textContent = 'COMP 3512';
+            party.textContent = '';
+
+            // append labels to the div
+            section.appendChild(credits);
+            section.appendChild(name);
+            section.appendChild(course);
+            section.appendChild(party);
+
  
             //hide header info after 5 sec
             setTimeout( function () {
-                header().style.display='none';
+                section.innerHTML = '';
             }, 5000);
         })
 
