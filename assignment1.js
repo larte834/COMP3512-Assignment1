cad0f679c678b60627a3a567a83438ebb672900f
Randy <rlam960@mtroyal.ca>
2021-03-03 22:43:51 -0700

    finally

--- assignment1.js
+++ assignment1.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const section = document.createElement('section');
     headerBox.appendChild(section);
 
-    headerBox.addEventListener('mouseenter', (e) => {
+    headerBox.addEventListener('mouseover', (e) => {
         section.style.display = "grid";
         //create elements to be displayed
         const credits = document.createElement('label');
@@ -56,9 +56,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let listOfCompanies = localStorage.getItem('listOfCompanies');
     if (!listOfCompanies) {
         animation.style.display = "flex";
+        console.log('1');
         fetch(url1)
             .then(response => {
                 animation.style.display = 'none';
+                console.log('2');
                 if (response.ok) { return response.json() }
                 else { return Promise.reject({ status: response.status, statusTest: response.statusText }) }
             })
@@ -66,87 +68,39 @@ document.addEventListener("DOMContentLoaded", function () {
                 // save local storage
                 let json = JSON.stringify(data);
                 localStorage.setItem('listOfCompanies', json);
-                coList.push(...data);
-                const goButton = document.createElement('button');
-                const clearButton = document.createElement('button');
-                goButton.setAttribute('id', 'goButton');
-                goButton.textContent = "GO";
-                clearButton.setAttribute('id', 'clearButton');
-                clearButton.textContent = "CLEAR";
-                companyList.appendChild(goButton);
-                companyList.appendChild(clearButton);
+                console.log('3');
             })
             .catch(err => console.log(err));
     }
 
-    displayList(coList);
 
-    function createHeader() {
-        const info = document.querySelector('div.a');
-        info.style.display = "grid";
-        //create elements to be displayed
-        const credits = document.createElement('label');
-        const name = document.createElement('label');
-        const course = document.createElement('label');
-        const party = document.createElement('label');
+    // create buttons for list 
+    const goButton = document.createElement('button');
+    const clearButton = document.createElement('button');
+    goButton.setAttribute('id', 'goButton');
+    goButton.textContent = "GO";
+    clearButton.setAttribute('id', 'clearButton');
+    clearButton.textContent = "CLEAR";
+    listBox.appendChild(goButton);
+    listBox.appendChild(clearButton);
 
-        //create text content
-        credits.textContent = "Credits:"
-        name.textContent = "Randy Lam & Lidiya Artemenko";
-        course.textContent = "Comp 3512";
-        party.textContent = "";
-
-        //append labels to the div
-        info.appendChild(credits);
-        info.appendChild(name);
-        info.appendChild(course);
-        info.appendChild(party);
-    }
-
-    headerBox.addEventListener('mouseover', (e) => {
-        //show header info
-        //header()
-        const section = document.createElement('section');
-        headerBox.appendChild(section);
-        section.style.display = "grid";
-        // create elements to be displayed
-        const credits = document.createElement('label');
-        const name = document.createElement('label');
-        const course = document.createElement('label');
-        const party = document.createElement('label');
-
-        // create text content
-        credits.textContent = 'Credits';
-        name.textContent = 'Randy Lam & Lidiya Artemenko';
-        course.textContent = 'COMP 3512';
-        party.textContent = '';
-
-        // append labels to the div
-        section.appendChild(credits);
-        section.appendChild(name);
-        section.appendChild(course);
-        section.appendChild(party);
-
-
-        //hide header info after 5 sec
-        setTimeout(function () {
-            section.innerHTML = '';
-        }, 5000);
-    })
+    parsedCompanyList= JSON.parse(listOfCompanies);
+    displayList(parsedCompanyList);
 
     function displayList(data) {
         const list = document.createElement('ul');
-        companyList.appendChild(list);
+        list.setAttribute('id', 'ul1');
+        listBox.appendChild(list);
         //displaying list of companies
         for (let d of data) {
             const companyName = document.createElement('li');
             companyName.textContent = d.name;
             list.appendChild(companyName);
-            addEventListener('click', displayCompanyInfo(d));
-            
+            //addEventListener('click', displayCompanyInfo(d));
+
         }
     }
-    function displayCompanyInfo (data) {
+    function displayCompanyInfo(data) {
         const h2 = document.createElement('h2');
         h2.textContent = "Company Information";
         companyInfoBox.appendChild(h2);
@@ -169,10 +123,42 @@ document.addEventListener("DOMContentLoaded", function () {
         description.textContent = data.description;
     }
 
-    function displayMap (latitude, longitude) {
+    function displayMap(latitude, longitude) {
         mapBox.style.height = '600px';
         map = new google.maps.Map(mapBox, {
-            center: {lat:latitude, lng: longitude}, zoom: 6
+            center: { lat: latitude, lng: longitude }, zoom: 6
         });
     }
+
+    // now set up keyboard event handlers
+    const searchBox = document.querySelector('.search');
+    const suggestions = document.querySelector('#filterList');
+    searchBox.addEventListener('keyup', displayMatches);
+
+    // handler for keyboard input
+    function displayMatches() {
+        // don't start matching until user has typed in two letters
+        if (this.value.length >= 2) {
+            const matches = findMatches(this.value, parsedCompanyList);
+
+            // first remove all existing options from list
+            suggestions.innerHTML = "";
+
+            // now add current suggestions to <datalist>
+            matches.forEach(c => {
+                var option = document.createElement('li');
+                option.textContent = c.name;
+                suggestions.appendChild(option);
+            })
+
+        }
+    }
+
+       // uses filter and regular expression to create list of matching movies
+   function findMatches(wordToMatch, company) {
+    return company.filter(obj => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return obj.name.match(regex);
+      });
+  }
 });
